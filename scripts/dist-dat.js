8ea3a92d55f8779aad08ca23abc0018116490a6c
Evan Rosson <git@erosson.org>
2021-01-09 15:38:46 -0500

    multilanguage support

---
+++ scripts/dist-dat.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const list = require('../dist/index_dat.json')
+
+const input = process.argv[process.argv.length-2]
+const output = process.argv[process.argv.length-1]
+const out = __dirname+'/../'+output
+const in_ = __dirname+'/../'+input
+const all = JSON.parse(fs.readFileSync(in_))
+
+fs.mkdirSync(out, {recursive: true})
+all.forEach(dat => {
+  // fs.writeFileSync(out+'/'+dat.filename+'.json', JSON.stringify(dat, null, 2))
+  fs.writeFileSync(out+'/'+dat.filename+'.json', JSON.stringify(dat))
+})
--- scripts/dist-index.js
+++ scripts/dist-index.js
@@ -1,17 +1,15 @@
 const fs = require('fs')
-const all = require('../dist/all.json')
 const list = require('../dist/index_dat.json')
+const langs = require('../dist/lang.json')
+const all = require('../dist/all.json')
 
-all.forEach(dat => {
-  // fs.writeFileSync(__dirname+'/../dist/dat/'+dat.filename+'.json', JSON.stringify(dat, null, 2))
-  fs.writeFileSync(__dirname+'/../dist/dat/'+dat.filename+'.json', JSON.stringify(dat))
-})
-
+const langdirs = Object.fromEntries(langs.map(lang => [lang, new Set(fs.readdirSync(__dirname+"/../dist/lang/"+lang))]))
 const indexByName = Object.fromEntries(all.map(dat => ([dat.filename, {
   filename: dat.filename,
   numHeaders: dat.header.length,
   numItems: dat.data.length,
   size: JSON.stringify(dat).length,
+  langs: langs.filter(lang => langdirs[lang].has(dat.filename+".json")),
 }])))
 const index = list.map(name => indexByName[name] || {filename: name, missing: true})
 // fs.writeFileSync(__dirname+'/../dist/index.json', JSON.stringify(index, null, 2))
