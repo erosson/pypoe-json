1ead5828f616f60d1fb3f1e9d18facecb5119d44
Evan Rosson <git@erosson.org>
2021-01-18 14:19:56 -0500

    more pretty json

--- scripts/pretty.sh
+++ scripts/pretty.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 cd "`dirname "$0"`/.."
 
-for MAXFILE in `find dist -name '*.dat.json' -type f`; do
+# for MAXFILE in `find dist -maxdepth 1 -name '*.json' -type f`; do
+for MAXFILE in `find dist -name '*.json' -type f -maxdepth 1` `find dist -name '*.dat.json' -type f`; do
   # https://unix.stackexchange.com/questions/19654/how-do-i-change-the-extension-of-multiple-files
   MINFILE="${MAXFILE%.json}.min.json"
   echo "$MAXFILE -> $MINFILE"
